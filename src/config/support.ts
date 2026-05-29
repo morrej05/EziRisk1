9aa126b2ce85138841a466343e0ba6c708534e4d
morrej05 <james.morrell1@gmail.com>
2026-05-29 09:50:11 +0100

    kjbjk

--- src/config/support.ts
+++ src/config/support.ts
@@ -7,11 +7,11 @@ export const SUPPORT_CONFIG = {
 export const PUBLIC_LEGAL_DETAILS = {
   operator: 'EziSoft Solutions',
   dataController: 'EziSoft Solutions',
-  icoRegistrationNumber: 'ICO:00014056799',
+  icoRegistrationNumber: '00014056799',
   contactEmail: SUPPORT_CONFIG.email,
   website: 'https://www.ezirisk.co.uk',
   phone: '+44 01904 922362',
-  footerStatement: `EziRisk is operated by EziSoft Solutions. ICO registration number: ICO:00014056799. Contact: ${SUPPORT_CONFIG.email} or +44 01904 922362.`,
+  footerStatement: `EziRisk is operated by EziSoft Solutions. ICO registration number: 00014056799. Contact: ${SUPPORT_CONFIG.email} or +44 01904 922362.`,
   operationalNote: 'Structured fire and risk reporting workflows for FRA, FSD and DSEAR.',
 } as const;
 
